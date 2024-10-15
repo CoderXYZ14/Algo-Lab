19433dad909782551857033bc72ba532a1569b79
CoderXYZ14 <s1404islam@gmail.com>
2024-10-16 00:46:55 +0530

    L7-1

---
+++ L7_BankersAlgo/bankers_algorithm_gtk.cpp
@@ -0,0 +1,262 @@
+#include <gtk/gtk.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+class BankersAlgorithm
+{
+private:
+    int numProcesses;
+    int numResources;
+    std::vector<std::vector<int>> allocation;
+    std::vector<std::vector<int>> maxNeed;
+    std::vector<int> available;
+    std::vector<std::vector<int>> need;
+
+    GtkWidget *window;
+    GtkWidget *processesEntry;
+    GtkWidget *resourcesEntry;
+    GtkWidget *allocationGrid;
+    GtkWidget *maxNeedGrid;
+    GtkWidget *availableGrid;
+    GtkWidget *resultLabel;
+    GtkTextBuffer *outputBuffer;
+    GtkWidget *outputView;
+
+public:
+    BankersAlgorithm() : numProcesses(0), numResources(0) {}
+
+    static void activate(GtkApplication *app, gpointer user_data)
+    {
+        BankersAlgorithm *bankersAlgo = static_cast<BankersAlgorithm *>(user_data);
+        bankersAlgo->createWindow(app);
+    }
+
+    void createWindow(GtkApplication *app)
+    {
+        window = gtk_application_window_new(app);
+        gtk_window_set_title(GTK_WINDOW(window), "Banker's Algorithm");
+        gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+
+        GtkWidget *grid = gtk_grid_new();
+        gtk_container_add(GTK_CONTAINER(window), grid);
+
+        createInputSection(grid);
+        createTableSection(grid);
+
+        gtk_widget_show_all(window);
+    }
+
+    void createInputSection(GtkWidget *grid)
+    {
+        gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Number of Processes:"), 0, 0, 1, 1);
+        processesEntry = gtk_entry_new();
+        gtk_grid_attach(GTK_GRID(grid), processesEntry, 1, 0, 1, 1);
+
+        gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Number of Resources:"), 2, 0, 1, 1);
+        resourcesEntry = gtk_entry_new();
+        gtk_grid_attach(GTK_GRID(grid), resourcesEntry, 3, 0, 1, 1);
+
+        GtkWidget *createTablesButton = gtk_button_new_with_label("Create Tables");
+        g_signal_connect(createTablesButton, "clicked", G_CALLBACK(onCreateTables), this);
+        gtk_grid_attach(GTK_GRID(grid), createTablesButton, 4, 0, 1, 1);
+    }
+
+    void createTableSection(GtkWidget *grid)
+    {
+        GtkWidget *tablesGrid = gtk_grid_new();
+        gtk_grid_set_column_spacing(GTK_GRID(tablesGrid), 10);
+        gtk_grid_attach(GTK_GRID(grid), tablesGrid, 0, 1, 5, 1);
+
+        allocationGrid = gtk_grid_new();
+        maxNeedGrid = gtk_grid_new();
+        availableGrid = gtk_grid_new();
+
+        gtk_grid_attach(GTK_GRID(tablesGrid), gtk_label_new("Allocation:"), 0, 0, 1, 1);
+        gtk_grid_attach(GTK_GRID(tablesGrid), allocationGrid, 0, 1, 1, 1);
+
+        gtk_grid_attach(GTK_GRID(tablesGrid), gtk_label_new("Max Need:"), 1, 0, 1, 1);
+        gtk_grid_attach(GTK_GRID(tablesGrid), maxNeedGrid, 1, 1, 1, 1);
+
+        gtk_grid_attach(GTK_GRID(tablesGrid), gtk_label_new("Available:"), 2, 0, 1, 1);
+        gtk_grid_attach(GTK_GRID(tablesGrid), availableGrid, 2, 1, 1, 1);
+
+        GtkWidget *calculateButton = gtk_button_new_with_label("Calculate");
+        g_signal_connect(calculateButton, "clicked", G_CALLBACK(onCalculate), this);
+        gtk_grid_attach(GTK_GRID(grid), calculateButton, 0, 2, 1, 1);
+
+        resultLabel = gtk_label_new("");
+        gtk_grid_attach(GTK_GRID(grid), resultLabel, 0, 3, 5, 1);
+
+        outputView = gtk_text_view_new();
+        outputBuffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(outputView));
+        GtkWidget *scrolledWindow = gtk_scrolled_window_new(NULL, NULL);
+        gtk_container_add(GTK_CONTAINER(scrolledWindow), outputView);
+        gtk_widget_set_size_request(scrolledWindow, -1, 200);
+        gtk_grid_attach(GTK_GRID(grid), scrolledWindow, 0, 4, 5, 1);
+    }
+
+    static void onCreateTables(GtkWidget *widget, gpointer data)
+    {
+        BankersAlgorithm *bankersAlgo = static_cast<BankersAlgorithm *>(data);
+        bankersAlgo->createTables();
+    }
+
+    void createTables()
+    {
+        numProcesses = std::stoi(gtk_entry_get_text(GTK_ENTRY(processesEntry)));
+        numResources = std::stoi(gtk_entry_get_text(GTK_ENTRY(resourcesEntry)));
+
+        setupTable(allocationGrid, "Allocation");
+        setupTable(maxNeedGrid, "Max Need");
+        setupTable(availableGrid, "Available", true);
+    }
+
+    void setupTable(GtkWidget *grid, const std::string &name, bool isAvailable = false)
+    {
+        GList *children, *iter;
+        children = gtk_container_get_children(GTK_CONTAINER(grid));
+        for (iter = children; iter != NULL; iter = g_list_next(iter))
+            gtk_widget_destroy(GTK_WIDGET(iter->data));
+        g_list_free(children);
+
+        int rows = isAvailable ? 1 : numProcesses;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < numResources; j++)
+            {
+                GtkWidget *entry = gtk_entry_new();
+                gtk_entry_set_width_chars(GTK_ENTRY(entry), 5);
+                gtk_grid_attach(GTK_GRID(grid), entry, j, i, 1, 1);
+            }
+        }
+
+        gtk_widget_show_all(grid);
+    }
+
+    static void onCalculate(GtkWidget *widget, gpointer data)
+    {
+        BankersAlgorithm *bankersAlgo = static_cast<BankersAlgorithm *>(data);
+        bankersAlgo->runBankersAlgorithm();
+    }
+
+    void runBankersAlgorithm()
+    {
+        getTableData();
+        calculateNeed();
+
+        std::vector<bool> finish(numProcesses, false);
+        std::vector<int> work = available;
+        std::vector<int> safeSequence;
+
+        while (safeSequence.size() < numProcesses)
+        {
+            bool found = false;
+            for (int i = 0; i < numProcesses; i++)
+            {
+                if (!finish[i] && canAllocate(i, work))
+                {
+                    for (int j = 0; j < numResources; j++)
+                    {
+                        work[j] += allocation[i][j];
+                    }
+                    safeSequence.push_back(i);
+                    finish[i] = true;
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                break;
+            }
+        }
+
+        std::stringstream output;
+        if (safeSequence.size() == numProcesses)
+        {
+            output << "Safe sequence: ";
+            for (size_t i = 0; i < safeSequence.size(); i++)
+            {
+                output << "P" << safeSequence[i];
+                if (i < safeSequence.size() - 1)
+                {
+                    output << " -> ";
+                }
+            }
+            output << "\nSystem is in a safe state.";
+            gtk_label_set_text(GTK_LABEL(resultLabel), "System is in a safe state.");
+        }
+        else
+        {
+            output << "System is in an unsafe state.";
+            gtk_label_set_text(GTK_LABEL(resultLabel), "System is in an unsafe state.");
+        }
+
+        gtk_text_buffer_set_text(outputBuffer, output.str().c_str(), -1);
+    }
+    bool canAllocate(int process, const std::vector<int> &work)
+    {
+        for (int i = 0; i < numResources; i++)
+        {
+            if (need[process][i] > work[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void getTableData()
+    {
+        allocation.clear();
+        maxNeed.clear();
+        available.clear();
+
+        for (int i = 0; i < numProcesses; i++)
+        {
+            std::vector<int> allocRow, maxRow;
+            for (int j = 0; j < numResources; j++)
+            {
+                GtkWidget *allocEntry = gtk_grid_get_child_at(GTK_GRID(allocationGrid), j, i);
+                GtkWidget *maxEntry = gtk_grid_get_child_at(GTK_GRID(maxNeedGrid), j, i);
+                allocRow.push_back(std::stoi(gtk_entry_get_text(GTK_ENTRY(allocEntry))));
+                maxRow.push_back(std::stoi(gtk_entry_get_text(GTK_ENTRY(maxEntry))));
+            }
+            allocation.push_back(allocRow);
+            maxNeed.push_back(maxRow);
+        }
+
+        for (int j = 0; j < numResources; j++)
+        {
+            GtkWidget *availEntry = gtk_grid_get_child_at(GTK_GRID(availableGrid), j, 0);
+            available.push_back(std::stoi(gtk_entry_get_text(GTK_ENTRY(availEntry))));
+        }
+    }
+    void calculateNeed()
+    {
+        need.clear();
+        need.resize(numProcesses, std::vector<int>(numResources));
+        for (int i = 0; i < numProcesses; i++)
+        {
+            for (int j = 0; j < numResources; j++)
+            {
+                need[i][j] = maxNeed[i][j] - allocation[i][j];
+            }
+        }
+    }
+};
+
+int main(int argc, char **argv)
+{
+    BankersAlgorithm bankersAlgo;
+
+    GtkApplication *app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(BankersAlgorithm::activate), &bankersAlgo);
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
+}
